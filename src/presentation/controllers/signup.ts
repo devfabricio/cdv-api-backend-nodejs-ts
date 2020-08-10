2a67917a55cfb8a8315216ca01efb866462b2a2f
Fabrício Augusto <fabricio.mata@gmail.com>
2020-08-10 15:40:48 -0300

    feat: ensure that SignupController returns error if no name is provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,14 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { badRequest } from '../helpers/http-helper'
+import { MissingParamError } from '../errors/missing-param-error'
+
+export class SignupController {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+  }
+}
